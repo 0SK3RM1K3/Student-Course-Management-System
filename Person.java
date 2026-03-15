b8e89855c43a3b03ef0ea07ba589e30d1e725aef
Anthony Sotera <s.terrainc@gmail.com>
2026-03-15 10:04:19 -0400

    Started person class, made fields and constructor

---
+++ Person.java
@@ -0,0 +1,15 @@
+// Class Person
+public abstract class Person {
+
+    //private fields
+    private String name;
+    private String id;
+    private String email;
+
+    //constructor
+    public Person(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
