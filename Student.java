4bd33a5c877363c746ef8cc369745201630c6aca
Anthony Sotera <s.terrainc@gmail.com>
2026-03-15 10:10:16 -0400

    started Student class, made list variable and constructor

--- Student.java
+++ Student.java
@@ -1,3 +1,14 @@
-public class Student {
-    
+import java.util.ArrayList;
+
+//Student class that extends Person
+public class Student extends Person {
+
+    //List to store courses
+    private ArrayList<String> courses;
+
+    //constructor
+    public Student(String name, String id, String email) {
+        super(name, id, email);
+        courses = new ArrayList<>();
+    }
 }
