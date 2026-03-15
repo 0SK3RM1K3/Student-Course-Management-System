1816b460a333b3d910b1639e4b29a4860775b902
Tyson Ward <tysonward109@gmail.com>
2026-03-15 11:10:09 -0400

    Used the validator class in the main to check if ID and Email are proper format

--- Main.java
+++ Main.java
@@ -6,7 +6,26 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
 
-        
+        try {
+
+            System.out.println("Enter name:");
+            String name = scanner.nextLine();
+
+            System.out.println("Enter Student ID(9 digits):");
+            String id= scanner.nextLine();
+
+            // Validate ID using Validator class 
+            if(!Validator.isValidStudentId(id)){
+                throw new IllegalArgumentException("Student ID must be exactly 9 digits.");
+            }
+            System.out.println("Enter Email:");
+            String email = scanner.nextLine();
+
+            //Validate email using the Validator class
+            if (!Validator.isValidEmail(email)){
+                throw new IllegalArgumentException("Invalid email format.");
+            }
+        }
     }
     
 }
