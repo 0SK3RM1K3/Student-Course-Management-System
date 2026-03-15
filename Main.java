25b3c7cf2026a32975efa5a83614195dbfc5dd5a
Tyson Ward <tysonward109@gmail.com>
2026-03-15 12:04:28 -0400

    Added Student menu

--- Main.java
+++ Main.java
@@ -10,4 +10,18 @@ public class Main {
         try (Scanner scanner = new Scanner(System.in)){
 
             boolean running = true;
-            
+
+            while(running){
+
+                //Student Menu System
+                System.out.println("\nStudent Menu");
+                System.out.println("\n1. Add Student");
+                System.out.println("\n2. Delete Student");
+                System.out.println("\n3. View Students");
+                System.out.println("\n4. Exit");
+                System.out.println("\nPlease choose an option");
+
+                int choice = Integer.parseInt(scanner.nextLine());
+
+
+            }
