b5db091a5319addc07042a55d410f28197d60578
Anthony Sotera <s.terrainc@gmail.com>
2026-03-15 10:15:45 -0400

    started Instructor class, created private field for department and constructor

--- Instructor.java
+++ Instructor.java
@@ -1,3 +1,11 @@
-public class Instructor {
-    
+public class Instructor extends Person {
+
+    //private field
+    private String department;
+
+    //constructor
+    public Instructor(String name, String id, String email, String department) {
+        super(name, id, email);
+        this.department = department;
+    }
 }
